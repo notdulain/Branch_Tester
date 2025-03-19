0c6d3c4092a110b1aff64fb495f2bb6971a43904
notdulain <03dulain@gmail.com>
2025-03-19 16:50:18 +0530

    menna aluth attac

--- branch.java
+++ branch.java
@@ -3,3 +3,9 @@ public class Branch {
         System.out.println("Hello World");
     }
 }
+
+public class Branch2 {
+    public static void main(String[] args) {
+        System.out.println("Hello Dulain Modaya");
+    }
+}
